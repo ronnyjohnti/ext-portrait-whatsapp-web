d0a8f1fcbb9c0c9bfca666fcd173cd8a83d6e28c
Ronny John <me@ronnyjohnti.dev>
2026-06-11 11:04:46 -0300

    fix: Fix selectors of HTML Elements after WhatsApp Web update

--- src/content/DOMElements.js
+++ src/content/DOMElements.js
@@ -1,4 +1,5 @@
-export const chatsColumn = () => document.querySelectorAll('div:has(> header [title])')[1];
-export const messagesColumn = () => chatsColumn().nextElementSibling;
-export const buttonChats = () => document.querySelector('button[aria-label]:has([data-icon=chats-filled])');
+export const chatsColumn = () => document.querySelector('div:has(> header[data-testid="chatlist-header"] [data-testid="drawer-title-body"])');
+export const messagesColumn = () => document.querySelector('div:has(> [data-testid="conversation-panel-wrapper"])')
+  || document.querySelector('div:has(> * > [data-testid="intro-panel"])');
+export const buttonChats = () => document.querySelectorAll('header[data-testid="chatlist-header"] div:has(>span>div>button[aria-label])')[0].querySelector('button[aria-label]');
 export const chatsList = () => document.querySelector('#pane-side > div > div > [aria-label]');
